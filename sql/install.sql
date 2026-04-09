17d2fcde6ea843d9c772342c93772d853817ef42
LinuaSacraMunda <el23077@mail.ntua.gr>
2026-04-09 14:39:47 +0300

    added hospitalisation, costing, insurance, diagnosis, triage tables; TODO shift triggers

--- sql/install.sql
+++ sql/install.sql
@@ -17,7 +17,7 @@ USE ntua_db_2026;
 --
 
 CREATE TABLE patient (
-    AMKA INT UNSIGNED NOT NULL,
+    AMKA VARCHAR(10) NOT NULL,
     first_name VARCHAR(45) NOT NULL,
     middle_name VARCHAR(45) DEFAULT NULL,
     last_name VARCHAR(45) NOT NULL,
@@ -38,18 +38,18 @@ CREATE TABLE patient (
 
     triage_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (AMKA),
-    --CONSTRAINT fk_patient_triage FOREIGN KEY (triage_id) REFERENCES triage (triage_id) ON DELETE RESTRICT ON UPFATE CASCADE
+    CONSTRAINT fk_patient_triage FOREIGN KEY (triage_id) REFERENCES triage (triage_id) ON DELETE RESTRICT ON UPFATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE patient_email (
-    AMKA INT UNSIGNED NOT NULL,
+    AMKA VARCHAR(10) NOT NULL,
     email_address VARCHAR(45) NOT NULL,
     PRIMARY KEY (AMKA, email_address),
     CONSTRAINT fk_patient_email FOREIGN KEY (AMKA) REFERENCES patient (AMKA) ON DELETE RESTRICT ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE patient_phone (
-    AMKA INT UNSIGNED NOT NULL,
+    AMKA VARCHAR(10) NOT NULL,
     phone_number VARCHAR(20) NOT NULL,
     PRIMARY KEY (AMKA, phone_number),
     CONSTRAINT fk_patient_phone FOREIGN KEY (AMKA) REFERENCES patient (AMKA) ON DELETE RESTRICT ON UPDATE CASCADE
@@ -61,7 +61,7 @@ CREATE TABLE patient_phone (
 --
 
 CREATE TABLE nurse (
-    AMKA INT UNSIGNED NOT NULL,
+    AMKA VARCHAR(10) NOT NULL,
     first_name VARCHAR(45) NOT NULL,
     middle_name VARCHAR(45) DEFAULT NULL,
     last_name VARCHAR(45) NOT NULL,
@@ -74,14 +74,14 @@ CREATE TABLE nurse (
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE nurse_email (
-    AMKA INT UNSIGNED NOT NULL,
+    AMKA VARCHAR(10) NOT NULL,
     email_address VARCHAR(45) NOT NULL,
     PRIMARY KEY (AMKA, email_address),
     CONSTRAINT fk_nurse_email FOREIGN KEY (AMKA) REFERENCES nurse (AMKA) ON DELETE RESTRICT ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE nurse_phone (
-    AMKA INT UNSIGNED NOT NULL,
+    AMKA VARCHAR(10) NOT NULL,
     phone_number VARCHAR(20) NOT NULL,
     PRIMARY KEY (AMKA, phone_number),
     CONSTRAINT fk_nurse_phone FOREIGN KEY (AMKA) REFERENCES nurse (AMKA) ON DELETE RESTRICT ON UPDATE CASCADE
@@ -93,7 +93,7 @@ CREATE TABLE nurse_phone (
 --
 
 CREATE TABLE administrative_staff (
-    AMKA INT UNSIGNED NOT NULL,
+    AMKA VARCHAR(10) NOT NULL,
     first_name VARCHAR(45) NOT NULL,
     middle_name VARCHAR(45) DEFAULT NULL,
     last_name VARCHAR(45) NOT NULL,
@@ -107,14 +107,14 @@ CREATE TABLE administrative_staff (
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE admin_email (
-    AMKA INT UNSIGNED NOT NULL,
+    AMKA VARCHAR(10) NOT NULL,
     email_address VARCHAR(45) NOT NULL,
     PRIMARY KEY (AMKA, email_address),
     CONSTRAINT fk_admin_email FOREIGN KEY (AMKA) REFERENCES administrative_staff (AMKA) ON DELETE RESTRICT ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE admin_phone (
-    AMKA INT UNSIGNED NOT NULL,
+    AMKA VARCHAR(10) NOT NULL,
     phone_number VARCHAR(20) NOT NULL,
     PRIMARY KEY (AMKA, phone_number),
     CONSTRAINT fk_admin_phone FOREIGN KEY (AMKA) REFERENCES administrative_staff (AMKA) ON DELETE RESTRICT ON UPDATE CASCADE
@@ -126,7 +126,7 @@ CREATE TABLE admin_phone (
 --
 
 CREATE TABLE doctor (
-    AMKA INT UNSIGNED NOT NULL,
+    AMKA VARCHAR(10) NOT NULL,
     first_name VARCHAR(45) NOT NULL,
     middle_name VARCHAR(45) DEFAULT NULL,
     last_name VARCHAR(45) NOT NULL,
@@ -140,14 +140,14 @@ CREATE TABLE doctor (
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE doctor_email (
-    AMKA INT UNSIGNED NOT NULL,
+    AMKA VARCHAR(10) NOT NULL,
     email_address VARCHAR(45) NOT NULL,
     PRIMARY KEY (AMKA, email_address),
     CONSTRAINT fk_doctor_email FOREIGN KEY (AMKA) REFERENCES doctor (AMKA) ON DELETE RESTRICT ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE doctor_phone (
-    AMKA INT UNSIGNED NOT NULL,
+    AMKA VARCHAR(10) NOT NULL,
     phone_number VARCHAR(20) NOT NULL,
     PRIMARY KEY (AMKA, phone_number),
     CONSTRAINT fk_doctor_phone FOREIGN KEY (AMKA) REFERENCES doctor (AMKA) ON DELETE RESTRICT ON UPDATE CASCADE
@@ -164,7 +164,7 @@ CREATE TABLE specialisation (
 
 
 CREATE TABLE doc_spec (
-    AMKA INT UNSIGNED NOT NULL,
+    AMKA VARCHAR(10) NOT NULL,
     spec_code VARCHAR(5) NOT NULL,
     PRIMARY KEY (AMKA, spec_code),
     CONSTRAINT fk_doctor_id FOREIGN KEY (AMKA) REFERENCES doctor (AMKA) ON DELETE RESTRICT ON UPDATE CASCADE,
@@ -253,7 +253,7 @@ CREATE TABLE department (
 --
 
 CREATE TABLE doctor_dept (
-    AMKA INT UNSIGNED NOT NULL,
+    AMKA VARCHAR(10) NOT NULL,
     dept_name INT UNSIGNED NOT NULL,
     PRIMARY KEY (AMKA, dept_name),
     CONSTRAINT fk_doc_dept_doctor_id FOREIGN KEY (AMKA) REFERENCES doctor (AMKA) ON DELETE RESTRICT ON UPDATE CASCADE,
@@ -330,7 +330,7 @@ CREATE TABLE shift (
 
 
 CREATE TABLE nurse_shift (
-    AMKA INT UNSIGNED NOT NULL,
+    AMKA VARCHAR(10) NOT NULL,
     shift_id INT UNSIGNED NOT NULL,
     PRIMARY KEY (AMKA, shift_id),
     CONSTRAINT fk_nurse_shift_nurse_id FOREIGN KEY (AMKA) REFERENCES nurse (AMKA) ON DELETE RESTRICT ON UPDATE CASCADE,
@@ -338,7 +338,7 @@ CREATE TABLE nurse_shift (
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE doctor_shift (
-    AMKA INT UNSIGNED NOT NULL,
+    AMKA VARCHAR(10) NOT NULL,
     shift_id INT UNSIGNED NOT NULL,
     PRIMARY KEY (AMKA, shift_id),
     CONSTRAINT fk_doctor_shift_doctor_id FOREIGN KEY (AMKA) REFERENCES doctor (AMKA) ON DELETE RESTRICT ON UPDATE CASCADE,
@@ -346,13 +346,149 @@ CREATE TABLE doctor_shift (
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE admin_shift (
-    AMKA INT UNSIGNED NOT NULL,
+    AMKA VARCHAR(10) NOT NULL,
     shift_id INT UNSIGNED NOT NULL,
     PRIMARY KEY (AMKA, shift_id),
     CONSTRAINT fk_admin_shift_admin_id FOREIGN KEY (AMKA) REFERENCES administrative_staff (AMKA) ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT fk_admin_shift_shift_id FOREIGN KEY (shift_id) REFERENCES shift (shift_id) ON DELETE RESTRICT ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+--  Table structure for triage
+--
+
+CREATE TABLE triage (
+    triage_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    level TINYINT(1) UNSIGNED NOT NULL CHECK (1 <= level AND level <= 5),
+    arrival_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    symptoms TEXT NOT NULL,
+    PRIMARY KEY (triage_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    
+
+--
+--  Table structure for insurance carrier 
+--
+
+CREATE TABLE insurance_carrier (
+    carrier_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(carrier_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE patient_insurance (
+    AMKA INT UNSIGNED NOT NULL,
+    carrier_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY(AMKA, carrier_id),
+    CONSTRAINT fk_patient_insurance_patient_id FOREIGN KEY (AMKA) REFERENCES patient (AMKA) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_patient_insurance_insurance_id FOREIGN KEY (carrier_id) REFERENCES insurance_carrier (carrier_id) ON DELETE RESTRICT ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+--  Table structure for hospitalisation
+--
+
+CREATE TABLE costing (
+    KEN VARCHAR(5) NOT NULL,
+    description TEXT,
+    base_cost NUMERIC(8,2) NOT NULL DEFAULT 000000.00,
+    mean_hospit_time INT UNSIGNED NOT NULL DEFAULT 0,
+    PRIMARY KEY (KEN),
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for relation costing ---< covered_by >--> insurance_carrier
+--
+
+
+CREATE TABLE costing_coverage (
+    KEN VARCHAR(5) NOT NULL,
+    carrier_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (KEN),
+    CONSTRAINT fk_const_coverage_costing_id FOREIGN KEY (KEN) REFERENCES costing (KEN) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_const_coverage_carrier_id FOREIGN KEY (carrier_id) REFERENCES insurance_carrier (carrier_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+--  Table structure for dianosis
+--      based on ICD-10 codes
+--
+
+CREATE TABLE diagnosis (
+    diag_id VARCHAR(10) NOT NULL,
+    discription TEXT NOT NULL,
+    PRIMARY KEY (diag_key)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+--  Table structure for hospitalisation
+--
+
+CREATE TABLE hospitalisation (
+    hosp_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    admission_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    discharge_date TIMESTAMP DEFAULT NULL,
+    dept_name VARCHAR(45) NOT NULL,
+    bed_id INT UNSIGNED NOT NULL,
+    KEN VARCHAR(7) NOT NULL,
+    PRIMARY KEY (hosp_id),
+    CONSTRAINT fk_hosp_dept_id FOREIGN KEY (dept_name) REFERENCES department (dept_name) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_hosp_bed_id FOREIGN KEY (bed_id) REFERENCES bed (bed_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_hosp_KEN FOREIGN KEY (KEN) REFERENCES costing (KEN) ON DELETE RESTRICT ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for relation hospitalisation ---< has_record >--> patient
+--
+
+CREATE TABLE patient_record (
+    AMKA INT UNSIGNED NOT NULL,
+    hosp_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (hosp_id),
+    CONSTRAINT fk_patient_record_patient_id FOREIGN KEY (AMKA) REFERENCES patient (AMKA) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_patient_record_hospitalisation_id FOREIGN KEY (hosp_id) REFERENCES hospitalisation (hosp_id) ON DELETE RESTRICT ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+--  Table structures for admission and discharge diagnosis
+--
+
+CREATE TABLE admission_diagnosis (
+    hosp_id INT UNSIGNED NOT NULL,
+    diag_id VARCHAR(10) NOT NULL,
+    PRIMARY KEY (hosp_id, diag_id),
+    CONSTRAINT fk_adm_diag_hospitalisation_id FOREIGN KEY (hosp_id) REFERENCES hospitalisation (hosp_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_adm_diag_diagnosis_id FOREIGN KEY (diag_id) REFERENCES diagnosis (diag_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE discharge_diagnosis (
+    hosp_id INT UNSIGNED NOT NULL,
+    diag_id VARCHAR(10) NOT NULL,
+    PRIMARY KEY (hosp_id, diag_id),
+    CONSTRAINT fk_dis_diag_hospitalisation_id FOREIGN KEY (hosp_id) REFERENCES hospitalisation (hosp_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_dis_diag_diagnosis_id FOREIGN KEY (diag_id) REFERENCES diagnosis (diag_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE tablename (
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- ===============================================================================
+--                                   Triggers
+-- ===============================================================================
 
 --
 -- Shift verification triggers
@@ -424,24 +560,30 @@ CREATE TRIGGER del_admin_shift BEFORE DELETE ON admin_shift FOR EACH ROW BEGIN
     END IF;
 END;;
 
+-- AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+--                          TODO
+-- AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+
 --
 -- Additional restrictions
--- ~ no staff member may have > 2 consecutive shifts
+-- ~ no staff member may have 2 consecutive shifts
 -- ~ no staff member may have > 3 consecutive night shifts
 --
 
 CREATE TRIGGER ins_doc_shift BEFORE INSERT ON doctor_shift FOR EACH ROW BEGIN
-    SELECT * FROM (
-        SELECT * FROM shift AS s 
-        WHERE s.AMKA = NEW.AMKA
-    ) AS hlpr WHERE DATE_SUB(NEW.)
+    SELECT *
+    FROM doctor_shift ds
+    INNER JOIN shift s on ds.shift_id  = s.shift_id
+    INNER JOIN doctor d on ds.AMKA = d.AMKA
+    WHERE d.AMKA = NEW.AMKA
+      AND DATE_ADD(NEW.day);
+
 END;;
 
 
 DELIMITER ;
 
-CREATE TABLE tablename (
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;

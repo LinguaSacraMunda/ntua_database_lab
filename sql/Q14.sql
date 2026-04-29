31378dddbe69668742d2dae850ed6ad028df0960
LinuaSacraMunda <el23077@mail.ntua.gr>
2026-04-29 13:29:41 +0300

    q14

---
+++ sql/Q14.sql
@@ -0,0 +1,17 @@
+WITH codes_yearly AS (
+    SELECT
+        YEAR(h.admission_date) AS year,
+        d.diag_id,
+        COUNT(*) AS cnt
+    FROM hospitalisation h
+    INNER JOIN admission_diagnosis ad ON ad.hosp_id = h.hosp_id
+    INNER JOIN discharge_diagnosis dd ON dd.hosp_id = h.hosp_id
+    INNER JOIN diagnosis d ON d.diag_id = ad.diag_id OR d.diag_id = dd.diag_id
+    GROUP BY year, d.diag_id)
+SELECT A.year, B.year AS prev_year, A.diag_id, A.cnt AS count
+FROM codes_yearly A
+INNER JOIN codes_yearly B 
+        ON A.year - 1 = B.year 
+       AND A.diag_id = B.diag_id
+       AND A.cnt = B.cnt
+WHERE A.cnt >= 5;
